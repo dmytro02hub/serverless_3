9a679dfe5d713c9afd4bc7b0a1ba93b628d86a7f
Dmytro Khomenko <d.khomenko@ukma.edu.ua>
2020-12-20 14:12:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React from "react";
+import { API, graphqlOperation, Auth, Hub } from "aws-amplify";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import { Router, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import { getUser } from "./graphql/queries";
+import { registerUser } from "./graphql/mutations";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MarketPage from "./pages/MarketPage";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+export const history = createBrowserHistory();
+
+export const UserContext = React.createContext();
+
+class App extends React.Component {
+  state = {
+    user: null,
+    userAttributes: {},
+  };
+
+  async componentDidMount() {
+    await this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+    console.dir(AmplifyTheme);
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user
+      ? this.setState({ user }, () => this.getUserAttributes(this.state.user))
+      : this.setState({ user: null });
+  };
+
+  getUserAttributes = async (authUserData) => {
+    const attributesArr = await Auth.userAttributes(authUserData);
+    const attributesObj = Auth.attributesToObject(attributesArr);
+
+    this.setState({ userAttributes: attributesObj });
+  };
+
+  onHubCapsule = (capsule) => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        this.getUserData();
+        this.registerNewUser(capsule.payload.data);
+        break;
+      case "signUp":
+        console.log("signed up");
+        break;
+      case "signOut":
+        console.log("signed out");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
+
+  registerNewUser = async (signInData) => {
+    const getUserInput = {
+      id: signInData.signInUserSession.idToken.payload.sub,
+    };
+    const { data } = await API.graphql(graphqlOperation(getUser, getUserInput));
+    // if we can't get a user (meaning the user hasn't been registered before,
+    // then we execute registerUsre)
+    if (!data.getUser) {
+      try {
+        const registerUserInput = {
+          ...getUserInput,
+          username: signInData.username,
+          email: signInData.signInUserSession.idToken.payload.email,
+          registered: true,
+        };
+
+        const newUser = await API.graphql(
+          graphqlOperation(registerUser, { input: registerUserInput })
+        );
+        console.log({ newUser });
+      } catch (error) {
+        console.error("Error registering new user", error);
+      }
+    }
+  };
+
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.error("Error siging out user", error);
+    }
+  };
+
+  render() {
+    const { user, userAttributes } = this.state;
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : (
+      <UserContext.Provider value={{ user, userAttributes }}>
+        <Router history={history}>
+          <>
+            {/* Navbar */}
+            <Navbar user={user} handleSignout={this.handleSignout} />
+            {/* Routes */}
+            <div className="app-container">
+              <Route exact path="/" component={HomePage} />
+              <Route
+                path="/profile"
+                component={() => (
+                  <ProfilePage user={user} userAttributes={userAttributes} />
+                )}
+              />
+              <Route
+                path="/markets/:marketId"
+                component={({ match }) => (
+                  <MarketPage
+                    marketId={match.params.marketId}
+                    user={user}
+                    userAttributes={userAttributes}
+                  />
+                )}
+              />
+            </div>
+          </>
+        </Router>
+      </UserContext.Provider>
+    );
+  }
+}
+console.log(AmplifyTheme.button);
+const theme = {
+  ...AmplifyTheme,
+  button: { ...AmplifyTheme.button, backgroundColor: "var(--amazonOrange)" },
+  sectionBody: { ...AmplifyTheme.sectionBody, padding: "5px" },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)",
+  },
+};
+
+// export default withAuthenticator(App, true,[],null,theme);
+export default App;
---
+++ src/utils/useAmplifyAuth.js
@@ -0,0 +1,151 @@
+import { useReducer, useState, useEffect } from "react";
+import { API, graphqlOperation, Auth, Hub } from "aws-amplify";
+import { getUser } from "../graphql/queries";
+import { registerUser } from "../graphql/mutations";
+import {
+  FETCH_DATA_INIT,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE,
+  FETCH_USER_ATTRIBUTES,
+  RESET_USER_DATA,
+} from "./constants";
+
+const amplifyAuthReducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_DATA_INIT:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        user: action.payload.user,
+        userAttributes: action.payload.userAttributes,
+      };
+    case FETCH_DATA_FAILURE:
+      return { ...state, isLoading: false, isError: true };
+    case FETCH_USER_ATTRIBUTES:
+      return { ...state, userAttributes: action.payload.userAttributes };
+    case RESET_USER_DATA:
+      return { ...state, user: null, userAttributes: {} };
+    default:
+      throw new Error();
+  }
+};
+
+const useAmplifyAuth = () => {
+  const initialState = {
+    isLoading: true,
+    isError: false,
+    user: null,
+    userAttributes: null,
+  };
+  const [state, dispatch] = useReducer(amplifyAuthReducer, initialState);
+  const [triggerFetch, setTriggerFetch] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchUserAttributes = async (authUserData) => {
+      const attributesArr = await Auth.userAttributes(authUserData);
+      const attributesObj = Auth.attributesToObject(attributesArr);
+      return attributesObj;
+    };
+
+    const fetchUser = async () => {
+      return Auth.currentAuthenticatedUser();
+    };
+
+    const fetchUserData = async () => {
+      if (isMounted) {
+        dispatch({ type: FETCH_DATA_INIT });
+      }
+      try {
+        const user = await fetchUser();
+        const userAttributes = await fetchUserAttributes(user);
+        if (userAttributes && isMounted) {
+          dispatch({
+            type: FETCH_DATA_SUCCESS,
+            payload: { user, userAttributes },
+          });
+        }
+      } catch (error) {
+        if (isMounted) {
+          dispatch({ type: FETCH_DATA_FAILURE });
+        }
+      }
+    };
+
+    const HubListener = () => {
+      Hub.listen("auth", (data) => {
+        const { payload } = data;
+        onAuthEvent(payload);
+      });
+    };
+
+    const onAuthEvent = (payload) => {
+      switch (payload.event) {
+        case "signIn":
+          if (isMounted) {
+            setTriggerFetch(true);
+            registerNewUser(payload.data);
+            console.log("signed in");
+          }
+          break;
+        default:
+          return;
+      }
+    };
+
+    HubListener();
+    fetchUserData();
+
+    return () => {
+      Hub.remove("auth");
+      isMounted = false;
+    };
+  }, [triggerFetch]);
+
+  const handleSignout = async () => {
+    try {
+      console.log("signed out");
+      await Auth.signOut();
+      setTriggerFetch(false);
+      dispatch({ type: RESET_USER_DATA });
+    } catch (error) {
+      console.error("Error signing out user ", error);
+    }
+  };
+
+  const registerNewUser = async (signInData) => {
+    const getUserInput = {
+      id: signInData.signInUserSession.idToken.payload.sub,
+    };
+    const { data } = await API.graphql(graphqlOperation(getUser, getUserInput));
+    // if user wasn't registered before, register them now
+    if (!data.getUser) {
+      try {
+        const registerUserInput = {
+          ...getUserInput,
+          username: signInData.username,
+          email: signInData.signInUserSession.idToken.payload.email,
+          registered: true,
+        };
+        const newUser = await API.graphql(
+          graphqlOperation(registerUser, { input: registerUserInput })
+        );
+        console.log({ newUser });
+      } catch (error) {
+        console.error("Error registering new user", error);
+      }
+    }
+  };
+
+  return { state, handleSignout };
+};
+
+export default useAmplifyAuth;
